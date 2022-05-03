7e4305df035041c896539702566a3b580990d2d0
Nikita Filonov <63447343+Nikita-Filonov@users.noreply.github.com>
2022-05-03 19:19:48 +0300

    Стеап

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+
+const CustomRoute = () => {
+  return (
+    <div>
+      asdsadsa
+    </div>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <CustomRoute/>
   </React.StrictMode>
 );
 
