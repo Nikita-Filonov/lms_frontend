63056a47c457fb4c14154ab91fc92993943a5594
Nikita Filonov <63447343+Nikita-Filonov@users.noreply.github.com>
2022-05-04 17:09:36 +0300

    Update CoursesProvider.js

--- src/Providers/CoursesProvider.js
+++ src/Providers/CoursesProvider.js
@@ -1,11 +1,18 @@
 import React, {useContext, useState} from 'react';
 import {get, patch, post} from "../Utils/Api/Fetch";
+import {store} from "../index";
+import {SET_COURSE} from "../Redux/Courses/actionTypes";
 
 const CoursesContext = React.createContext(null);
 
 
 const CoursesProvider = ({children}) => {
   const [courses, setCourses] = useState([]);
+
+  const getCourse = async (courseId: number) => {
+    const {error, json} = await get(`/courses/${courseId}/`, true);
+    !error && store.dispatch({type: SET_COURSE, payload: json});
+  }
 
   const getCourses = async () => {
     const {json, error} = await get('/courses/', true);
@@ -17,13 +24,13 @@ const CoursesProvider = ({children}) => {
     !error && setCourses([...courses, json]);
   };
 
-  const updateCourse = async (payload) => {
-    const {json, error} = await patch('/courses/', payload, true);
+  const updateCourse = async (courseId, payload) => {
+    const {json, error} = await patch(`/courses/${courseId}/`, payload, true);
     console.log(json, error)
   }
 
   return (
-    <CoursesContext.Provider value={{courses, getCourses, createCourse, updateCourse}}>
+    <CoursesContext.Provider value={{courses, getCourse, getCourses, createCourse, updateCourse}}>
       {children}
     </CoursesContext.Provider>
   );
